f45238df3f66230b431f19e249c258f7c08319b7
Felipe de Souza Paiva <spaiva.felipe@gmail.com>
2017-06-13 15:37:39 -0300

    Versao Inicial com 1 modulo

---
+++ MRINodeMCU.ino
@@ -0,0 +1,104 @@
+#include <ESP8266WiFi.h>
+
+ 
+const char* ssid = "Salvador-Sala";
+const char* password = "salvador1007";
+
+IPAddress ip(192,168,100,35);  //Node static IP
+IPAddress gateway(192,168,100,1);
+IPAddress subnet(255,255,255,0); 
+ 
+int ledPin = 13; // GPIO13
+WiFiServer server(80);
+ 
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT); 
+  
+  Serial.begin(115200);
+  delay(10);
+ 
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+ 
+  // Connect to WiFi network
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+ 
+  WiFi.begin(ssid, password);
+  WiFi.config(ip, gateway, subnet);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+ 
+  // Start the server
+  server.begin();
+  Serial.println("Server started");
+ 
+  // Print the IP address
+  Serial.print("Use this URL to connect: ");
+  Serial.print("http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("/");
+ 
+}
+ 
+void loop() {
+   digitalWrite(LED_BUILTIN, HIGH);
+
+  // Check if a client has connected
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
+  }
+ 
+  // Wait until the client sends some data
+  Serial.println("new client");
+  while(!client.available()){
+    delay(1);
+  }
+ 
+  // Read the first line of the request
+  String request = client.readStringUntil('\r');
+  Serial.println(request);
+  client.flush();
+ 
+  // Match the request
+ 
+  int value = LOW;
+  if (request.indexOf("/LED=ON") != -1)  {
+    digitalWrite(ledPin, HIGH);
+    value = HIGH;
+  }
+  if (request.indexOf("/LED=OFF") != -1)  {
+    digitalWrite(ledPin, LOW);
+    value = LOW;
+  }
+
+  // Return the response
+  client.println("HTTP/1.1 200 OK");
+  client.println("Content-Type: text/html");
+  client.println(""); //  do not forget this one
+  client.println("<!DOCTYPE HTML>");
+  client.println("<html>");
+ 
+  client.print("Led pin is now: ");
+ 
+  if(value == HIGH) {
+    client.print("On");
+  } else {
+    client.print("Off");
+  }
+    client.println("</html>");
+ 
+  delay(1);
+  Serial.println("Client disonnected");
+  Serial.println("");
+}
+ 
